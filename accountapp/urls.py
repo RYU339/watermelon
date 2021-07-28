dbdc7cb88a0c7460bebcd3065c5f8b722504e99f
RYU <ryuryuryu339@likelion.org>
2021-07-28 23:31:30 +0900

    21.07.28 Watermelon port

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from accountapp.views import watermelon
+from accountapp.views import tahlee
 
 app_name = 'accountapp'
 
 urlpatterns = [
-    path('watermelon/', watermelon, name='watermelon')
+    path('tahlee/', tahlee, name='tahlee')
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def watermelon(request):
+def tahlee(request):
     return HttpResponse('Watermelon')
