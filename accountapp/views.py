b9116c8ae29c86e3bcad5742e44ac11f6a89cbbf
RYU <ryuryuryu339@likelion.org>
2021-08-07 14:14:24 +0900

    21.08.07 Watermelon migration GET POST

--- accountapp/views.py
+++ accountapp/views.py
@@ -4,4 +4,9 @@ from django.shortcuts import render
 # Create your views here.
 
 def tahlee(request):
-    return render(request, 'accountapp/tahlee.html')
+
+    if request.method == 'POST':
+        return render(request, 'accountapp/tahlee.html', context={'text':'POST METHOD'})
+    # CONTEXT = 맥락, 데이터 꾸러미, test: 이름, post method: 내용물 text를 html 안에 넣어서 보낼 것
+    else:
+        return render(request, 'accountapp/tahlee.html', context={'text': 'GET METHOD'})
