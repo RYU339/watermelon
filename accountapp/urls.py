afba7d445d844634f961ffb3159d936dfc5e6ad3
RYU <ryuryuryu339@likelion.org>
2021-08-13 16:38:27 +0900

    21.08.13 Watermelon migration CreateView

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,9 +1,12 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accountapp.views import tahlee
+from accountapp.views import tahlee, AccountCreateView
 
 app_name = 'accountapp'
 
 urlpatterns = [
-    path('tahlee/', tahlee, name='tahlee')
+    path('tahlee/', tahlee, name='tahlee'),
+    path('create/', AccountCreateView.as_view(), name='create'),
+
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,14 +1,18 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 
 from accountapp.models import NewModel
 
 
 def tahlee(request):
     if request.method == 'POST':
+
         temp = request.POST.get('input_text')
 
         new_model = NewModel()  # (): NewModel에서 나온 새로운 객체가 new_model로 저장됨
@@ -20,3 +24,10 @@ def tahlee(request):
     else:
         data_list = NewModel.objects.all()
         return render(request, 'accountapp/tahlee.html', context={'data_list': data_list})
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:tahlee')
+    template_name = 'accountapp/create.html'
+
