e4d1bb130dd83ad40d6162e47a9fde6e47311192
RYU <ryuryuryu339@likelion.org>
2021-08-26 00:51:45 +0900

    21.08.25 Watermelon Authentication

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponseForbidden
 from django.shortcuts import render
 
 # Create your views here.
@@ -12,19 +12,22 @@ from accountapp.models import NewModel
 
 
 def tahlee(request):
-    if request.method == 'POST':
+    if request.user.is_authenticated:
+        if request.method == 'POST':
 
-        temp = request.POST.get('input_text')
+            temp = request.POST.get('input_text')
 
-        new_model = NewModel()  # (): NewModel에서 나온 새로운 객체가 new_model로 저장됨
-        new_model.text = temp
-        new_model.save()
+            new_model = NewModel()  # (): NewModel에서 나온 새로운 객체가 new_model로 저장됨
+            new_model.text = temp
+            new_model.save()
 
-        return HttpResponseRedirect(reverse('accountapp:tahlee'))
-    # CONTEXT = 맥락, 데이터 꾸러미, test: 이름, post method: 내용물 text를 html 안에 넣어서 보낼 것
+            return HttpResponseRedirect(reverse('accountapp:tahlee'))
+        # CONTEXT = 맥락, 데이터 꾸러미, test: 이름, post method: 내용물 text를 html 안에 넣어서 보낼 것
+        else:
+            data_list = NewModel.objects.all()
+            return render(request, 'accountapp/tahlee.html', context={'data_list': data_list})
     else:
-        data_list = NewModel.objects.all()
-        return render(request, 'accountapp/tahlee.html', context={'data_list': data_list})
+        return HttpResponseRedirect(reverse('accountapp:login'))
 
 class AccountCreateView(CreateView):
     model = User
@@ -44,8 +47,32 @@ class AccountUpdateView(UpdateView):
     success_url = reverse_lazy('accountapp:tahlee')
     template_name = 'accountapp/update.html'
 
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated and self.get_object() == request.user:
+            return super().get(request, *args, **kwargs)
+        else:
+            return HttpResponseForbidden()
+
+    def post(self, request, *args, **kwargs):
+        if request.user.is_authenticated and self.get_object() == request.user:
+            return super().post(request, *args, **kwargs)
+        else:
+            return HttpResponseForbidden()
+
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
     success_url = reverse_lazy('accountapp:tahlee')
     template_name = 'accountapp/delete.html'
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated and self.get_object() == request.user:
+            return super().get(request, *args, **kwargs)
+        else:
+            return HttpResponseForbidden()
+
+    def post(self, request, *args, **kwargs):
+        if request.user.is_authenticated and self.get_object() == request.user:
+            return super().post(request, *args, **kwargs)
+        else:
+            return HttpResponseForbidden()
